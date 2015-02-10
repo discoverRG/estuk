2aa2009a512889ffed03756ad74d073955c84010
Randy Greene <randy@discoverRG.com>
2015-02-09 20:20:44 -0600

    Add books scaffold and styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208221456) do
+ActiveRecord::Schema.define(version: 20150210010112) do
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availability"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
