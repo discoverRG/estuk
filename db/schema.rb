f602750f36b772050f955131cf2e702ccee509d8
Randy Greene <randy@discoverRG.com>
2015-02-09 20:41:45 -0600

    Add book/user association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210010112) do
+ActiveRecord::Schema.define(version: 20150210022344) do
 
   create_table "books", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150210010112) do
     t.boolean  "availability"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
